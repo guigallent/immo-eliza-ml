0c96c82d5cd690195c3fb21fe9ed0b8ca7078397
Guillermo Gallent <guillermo.gallent@gmail.com>
2026-07-01 17:28:16 +0200

    Include console output, changed decision_tree's max_depth to 8 and random_forest n_estimators to 200

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ from src.preprocess import preprocess_data
 from src.train import split_data, scale_data, linear_regression, decision_tree_regression, random_forest_regression, xgboost_regression
 from src.evaluate import evaluate_model
 
-# Reproducibility — always set a random seed!
-np.random.seed(RANDOM_STATE)
-
 df = pd.read_csv("./data/properties_database.csv")
 
 df_cleaned = clean_data(df)
@@ -23,14 +20,18 @@ X_train_scaled, X_test_scaled, scaler = scale_data(X_train_processed, X_test_pro
 
 linear = linear_regression(X_train_scaled, y_train)
 joblib.dump(linear, "models/linear.joblib")
+print("Linear Regression model saved as 'models/linear.joblib'.")
 
-decision_tree = decision_tree_regression(X_train_scaled, y_train, max_depth=3)
+decision_tree = decision_tree_regression(X_train_scaled, y_train, max_depth=8)
 joblib.dump(decision_tree, "models/decision_tree.joblib")
+print("Decision Tree Regressor model saved as 'models/decision_tree.joblib'.")
 
-random_forest = random_forest_regression(X_train_scaled, y_train, n_estimators=100)
+random_forest = random_forest_regression(X_train_scaled, y_train, n_estimators=200)
 joblib.dump(random_forest, "models/random_forest.joblib")
+print("Random Forest Regressor model saved as 'models/random_forest.joblib'.")
 
 xgboost = xgboost_regression(X_train_scaled, y_train, n_estimators=200, max_depth=6, learning_rate=0.05)
 joblib.dump(xgboost, "models/xgboost.joblib")
+print("XGBoost Regressor model saved as 'models/xgboost.joblib'.")
 
 #evaluate_model(model, X_test_scaled, y_test)
