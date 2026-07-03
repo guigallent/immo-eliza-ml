172931d40ee69688354f03975ce54ead9c201b48
Guillermo Gallent <guillermo.gallent@gmail.com>
2026-07-03 09:20:22 +0200

    Fix random_state inconsistency in split_data()

--- src/train.py
+++ src/train.py
@@ -29,7 +29,7 @@ def split_data(df: pd.DataFrame, test_size: float = 0.2, random_state: int = RAN
     X = df.drop("price", axis=1)
     y = df["price"]
     
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=RANDOM_STATE)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
     
     return X_train, X_test, y_train, y_test
 
