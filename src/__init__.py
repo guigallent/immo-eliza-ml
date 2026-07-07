715b686df0d45ec308369eff0102729e31f1a609
Guillermo Gallent <guillermo.gallent@gmail.com>
2026-07-07 12:12:41 +0200

    Update imports

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 from src.config import RANDOM_STATE
 from src.clean import clean_data
-from src.preprocess import preprocess_data
+from src.preprocess import PropertyPreprocessor
 from src.train import split_data, scale_data, linear_regression, decision_tree_regression, random_forest_regression, xgboost_regression
 from src.evaluate import evaluate_model, cross_validate_model, print_evaluation, print_cross_validation_results
 
